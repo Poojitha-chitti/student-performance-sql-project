ecf48f94833e51390696aea67e618506fd482da7
Poojitha Parasanamyna <poojipoojitha282@gmail.com>
2026-06-21 14:05:15 +0530

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,4 @@
+--Ranking Students
+SELECT name,marks,DENSE_RANK() OVER (ORDER BY marks DESC) AS rnk FROM Students;
+--Topper in each city
+SELECT * FROM (SELECT name,city,marks,DENSE_RANK() OVER (PARTITION BY city ORDER BY marks DESC) AS rnk FROM Students) t WHERE rnk=1;
