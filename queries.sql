8cb3353a530435f9af3326d49b92dbb8af939a2c
Poojitha Parasanamyna <poojipoojitha282@gmail.com>
2026-06-21 14:31:23 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,3 +2,5 @@
 SELECT name,marks,DENSE_RANK() OVER (ORDER BY marks DESC) AS rnk FROM Students;
 --Topper in each city
 SELECT * FROM (SELECT name,city,marks,DENSE_RANK() OVER (PARTITION BY city ORDER BY marks DESC) AS rnk FROM Students) t WHERE rnk=1;
+--Comparing marks with previous marks 
+SELECT name,marks,marks-LAG(marks) OVER (ORDER BY marks DESC) AS diff FROM Students;
